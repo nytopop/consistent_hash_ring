4589402d575cdb87e923d4ffb514ad83f1b76cd0
nytopop <ericizoita@gmail.com>
2019-12-30 22:21:57 -0800

    result_map_or_else has been stabilized

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
-
-#![feature(test, result_map_or_else)]
+#![feature(test)]
 
 extern crate fnv;
 
